feb5662347bc14298b4fe2014508bf40b445e599
Anjruski <league@iMac-2.attlocal.net>
2018-06-28 14:51:46 -0700

    Day 4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,38 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+public static void main(String[] args) {
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+Random R = new Random();
+int rand = R.nextInt(4);
 	// 3. Print out this variable
-
+System.out.println(rand);
 	// 4. Get the user to enter something that they think is awesome
-
+JOptionPane.showInputDialog("What do you think is awesome?");
 	// 5. If the random number is 0
-
+if (rand==0) { JOptionPane.showMessageDialog(null, "That is awesome!");
 	// -- tell the user whatever they entered is awesome!
-
+}
 	// 6. If the random number is 1
-
-	// -- tell the user whatever they entered is ok.
-
+if (rand==1) { JOptionPane.showMessageDialog(null, "That is OK.");
+	// -- tell the user whatever they entered is OK.
+}
 	// 7. If the random number is 2
-
+if (rand==2) { JOptionPane.showMessageDialog(null, "That is boring.");
 	// -- tell the user whatever they entered is boring.
-
+}
 	// 8. If the random number is 3
-
+if (rand==3) { JOptionPane.showMessageDialog(null, ":/");
 	// -- write your own answer
-
+}
 }
 
 
+}
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,36 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
-	
+
 	public static void main(String[] args) {
-		
-		// 1.  Create a variable to hold the user's score 
-		
-		// 2.  Ask the user a question 
-		
-		// 3.  Use an if statement to check if their answer is correct
-		
-		// 4.  if the user's answer is correct
-		
-		// -- add one to their score 
-		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
-		// 6.  After all the questions have been asked, print the user's score 
-		
+		JOptionPane.showMessageDialog(null, "Fortnite Related Questions");
+		// 1. Create a variable to hold the user's score
+		int score = 0;
+		// 2. Ask the user a question
+		String input = JOptionPane.showInputDialog(
+				"If there are 8 trees around you and each tree gives 14 wooden logs, how many trees have to die if you want to get up a slope with 4 ramps of 10 wooden logs?");
+		// 3. Use an if statement to check if their answer is correct
+		if (input.equals("3")) {
+			// 4. if the user's answer is correct
+			score++;
+			// -- add one to their score
+		}
+		// 5. Create more questions by repeating steps 1, 2, and 3 below.
+		String input2 = JOptionPane.showInputDialog(
+				"If a tree has 600 health and your axe does 50 health each swing, how many swings will it take to finish the tree.");
+		if (input2.equals("12")) {
+			score++;
+		}
+		String input3 = JOptionPane.showInputDialog(
+				"If you are trying to get from one side to the other, and the storm is going to start to get you in 2 hours,\n and you have to run 20 miles at 5 MPH when the storm is coming at you at 10 MPH, how early do you have to start running before the storm catches you?  ");
+		if (input3.equals("2 hours before")) {
+			score++;
+			// 6. After all the questions have been asked, print the user's score
+			
+		}
+		JOptionPane.showMessageDialog(null, "Congratulations! you got "+score);
 	}
+
 }
